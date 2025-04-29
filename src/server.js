afa472a119274168c2a461e3554f9bfcac4a1111
saifulabidin <syaiful.osd@yahoo.com>
2025-04-29 23:36:11 +0700

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,19 +1,26 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
+/**
+ * @description Fungsi untuk menginisialisasi server Hapi
+ * @returns {Promise<void>}
+ */
 const init = async () => {
+  // Konfigurasi server dengan port dan host yang sesuai
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ['*'], // Mengizinkan akses dari semua origin
       },
     },
   });
 
+  // Mendaftarkan routes
   server.route(routes);
 
+  // Memulai server
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
